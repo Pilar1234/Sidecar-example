553a09b9b17d6236b7d2926ea613b3d02d3d44e0
ppilarcz <ppilarcz@jaguarlandrover.com>
2023-08-06 15:13:16 +0100

    initial commit

---
+++ main-app/src/main/java/com/example/mainapp/TestController.java
@@ -0,0 +1,18 @@
+package com.example.mainapp;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class TestController {
+
+  @GetMapping("/sampleTest")
+  public ResponseEntity<String> sample() {
+    return new ResponseEntity<>("Hello from sample controller!" ,HttpStatus.OK);
+  }
+}
+
---
+++ sidecar-app/src/test/java/com/example/sidecarapp/SidecarAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sidecarapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidecarAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
